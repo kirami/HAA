e8b63c828e15264fa52826128690b8e29f25167b
Kira DeValle <kirajmd@gmail.com>
2014-08-25 22:56:28 -0700

    added bids

---
+++ audio/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from audio.models import Address, Bid
+
+# Create the form class.
+class ContactForm(ModelForm):
+
+	class Meta:
+		model = Address
+		exclude = ('user',)
+		#fields = ['address_one', 'address_two', 'city', 'state', 'zipcode', 'postal_code', 'country', 'telephone', ]
+
+class BidSubmitForm(ModelForm):
+
+	class Meta:
+		model = Bid
+		#fields = ['address_one', 'address_two', 'city', 'state', 'zipcode', 'postal_code', 'country', 'telephone', ]
