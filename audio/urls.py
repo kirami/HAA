1de263329db6cfaf9b449e19eda75f72c04a5bbb
root <root@ip-172-31-41-68.us-west-2.compute.internal>
2014-12-03 00:17:03 +0000

    change password

--- audio/urls.py
+++ audio/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, url, include
-from django.contrib.auth.views import login, logout
-
+from django.contrib.auth.views import login, logout, password_change, password_change_done
 from django.contrib import admin
 from audio import views
 from audio import admin_views
@@ -11,6 +10,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
     url(r'^accounts/login/$',  login),
+    url(r'^accounts/changePassword/$',  password_change, {'template_name': 'changePassword.html'}),
+    url(r'^acounts/changePasswordDone/$',
+                    password_change_done,
+                    name='password_change_done'),
    	#url(r'^accounts/logout/$', logout),
     (r'^accounts/logout/$', logout,
                           {'next_page': '/audio/accounts/profile'}),
@@ -18,6 +21,7 @@ urlpatterns = patterns('',
    	url(r'^accounts/register/$', views.register, name='register'),
     url(r'^accounts/contact/$', views.contact_info, name='contact_info'), 
     url(r'^accounts/bids/$', views.bids, name='bids'),
+    url(r'^accounts/userInfo/$', views.userInfo, name='userInfo'),
     
     url(r'^catalog/(?P<auctionId>\d+)/(?P<lotId>\d+)$', views.showItem, name='showItem'),
     url(r'^catalog/submitBid', views.submitBid, name='submitBid'),
